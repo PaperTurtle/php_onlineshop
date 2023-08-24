0df4c637d7f5bb888aa8a83071f1a9352e21e0d1
Seweryn Czabanowski <seweryn.cz@outlook.com>
2023-08-24 12:10:11 +0200

    Removed unnecessary comments

--- js/mehrBestellungen.js
+++ js/mehrBestellungen.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
 	$("#load_more").click(function () {
 		const start = parseInt($("#start").val());
-
-		// Show the spinner while loading
 		$("#accordionFlushExample").append(
 			'<div class="d-flex pt-3 justify-content-center"> <div class = "spinner-border" role = "status" ><span class = "visually-hidden" > Loading... < /span> </div> </div>'
 		);
@@ -12,15 +10,12 @@ $(document).ready(function () {
 			method: "GET",
 			success: function (data) {
 				setTimeout(function () {
-					// Remove the entire spinner div and its content
 					$(".spinner-border").parent().remove();
 
-					// Append the new orders
 					$("#accordionFlushExample").append(data);
 
-					// Increment the offset
 					$("#start").val(start + 5);
-				}, 500); // Set the delay in milliseconds
+				}, 500);
 			},
 		});
 	});
