84cd0ff6f793e2fa0fdb2841a6b6164e096d8b4c
Seweryn Czabanowski <seweryn.cz@outlook.com>
2023-09-02 13:55:34 +0200

    Removed unnecessary php code

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer-Template -->
 <footer class="bg-dark text-white text-center py-4 mt-auto pt-4">
-    <p><small>&copy; <?= date("Y"); ?> Geschmacksgarten-Onlineshop, <span data-bs-toggle="tooltip" data-bs-placement="top" title=<?php echo "Yoooooo" ?>>Seweryn Czabanowski</span></small></p>
+    <p><small>&copy; <?= date("Y"); ?> Geschmacksgarten-Onlineshop, <span data-bs-toggle="tooltip" data-bs-placement="top" title="Yoooooo ">Seweryn Czabanowski</span></small></p>
 </footer>
 <script type="text/javascript" src="./js/mdb.min.js"></script>
 <script type="text/javascript" src="./js/customValidation.js"></script>
